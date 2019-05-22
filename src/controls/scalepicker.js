eddb37d4a6fbff6ee9d326ef0ad1f531cc321031
Iman Tahriri <iman_tahriri@yahoo.com>
2019-05-22 17:09:16 +0200

    scale picker wrapperd in Collapse component

--- src/controls/scalepicker.js
+++ src/controls/scalepicker.js
@@ -1,4 +1,5 @@
 import { Component, Button, Element as El, Collapse, dom } from '../ui';
+import mapUtils from '../maputils';
 
 const Scalepicker = function Scalepicker({
   closeIcon = '#ic_close_24px',
@@ -6,32 +7,104 @@ const Scalepicker = function Scalepicker({
 } = {}) {
   let headerComponent;
   let contentComponent;
+  let headerButton;
+  let map;
   let viewer;
   let target;
+  let projection;
+  let buttons;
+  let resolutions;
+  let scalepicker;
 
-  const testComponent = El({
-    tagName: 'div',
-    cls: 'flex column',
-    innerHTML: '<div>Hej</div>'
+  const initButtons = () => {
+    buttons = resolutions.map(resolution => {
+      return Button({
+        cls: 'padding-small icon-smaller rounded light flex column spacing-vertical-small',
+        click() {
+          const scaleValue = mapUtils.resolutionToScale(resolution, projection);
+          map.getView().setResolution(mapUtils.scaleToResolution(scaleValue, projection));
+        },
+        text: `1:${roundScale(mapUtils.resolutionToScale(resolution, projection))}`
+      });
+    });
+  }
+
+  const toggle = function toggle() {
+    if (headerButton.getState() === 'hidden') {
+      headerButton.setState('initial');
+      // closeButton.setState('hidden');
+    } else {
+      headerButton.setState('hidden');
+      // closeButton.setState('initial');
+    }
+    const customEvt = new CustomEvent('collapse:toggle', {
+      bubbles: true
+    });
+    document.getElementById(headerButton.getId()).dispatchEvent(customEvt);
+  };
+
+  const roundScale = (scale) => {
+    const differens = scale % 10; //is the number not even? 
+    if (differens != 0) {
+      scale = scale + (10 - differens);
+    }
+    return scale;
+  }
+
+  headerButton = Button({
+    cls: 'padding-small icon-smaller light text-normal',
+    text: 'ScalePicker',
+    icon: '#fa-caret-down',
+    tooltipText: 'Meny',
+    tooltipPlacement: 'west',
+    state: 'initial',
+    validStates: ['initial', 'hidden'],
+    click() {
+      toggle();
+    }
+  });
+
+  headerComponent = El({
+    cls: 'flex row justify-center',
+    style: { width: '100%' },
+    components: [headerButton]
+  });
+
+  contentComponent = Component({
+    render() {
+      return `<div class="relative width-8" id="${this.getId()}">${buttons.map(button => {
+        return button.render();
+      }).join('')}</div>`;
+    }
+  });
+
+  scalepicker = Collapse({
+    cls: 'absolute flex column top-left rounded box-shadow bg-white overflow-hidden',
+    collapseX: false,
+    headerComponent,
+    contentComponent
   });
 
   return Component({
     name: 'scalepicker',
     onAdd(evt) {
       viewer = evt.target;
+      map = viewer.getMap();
+      // resolutions = map.getView().getResolutions();
+      resolutions = [...viewer.getResolutions()].reverse();
+      projection = map.getView().getProjection();
       target = `${viewer.getMain().getId()}`;
-      this.addComponents([testComponent]);
+      // target = `${viewer.getMain().getNavigation().getId()}`;
+      initButtons();
+      this.addComponents([scalepicker]);
       this.render();
     },
     onInit() {
-      console.log('inting Scale Picker');
     },
     render() {
-      console.log('rendering scalepicker');
-      const elStr = testComponent.render();
-      const menuEl = dom.html(elStr);
-      // const menuEl = dom.html(`<div class="relative width-12"><ul class="padding-y-small" id="${this.getId()}"">Scale picker</ul></div>`);
-      document.getElementById(target).appendChild(menuEl);
+      const htmlString = scalepicker.render();
+      const el = dom.html(htmlString);
+      document.getElementById(target).appendChild(el);
       this.dispatch('render');
     }
   });
